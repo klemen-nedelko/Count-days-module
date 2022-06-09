10e696339f63998bd9194e796eeeb04d0887ad6f
klemen-nedelko <klemen.nedelko2@gmail.com>
2022-06-09 22:11:37 +0200

    Initial commit

--- src/Plugin/Block/CountDays.php
+++ src/Plugin/Block/CountDays.php
@@ -16,71 +16,25 @@ use Drupal\Core\Plugin;
   */
      class CountDays extends BlockBase 
      {
+         protected $countDays;
+         protected $node;
+         public function __construct(){
+            $this->countDays = \Drupal::service('count_days.countDays');
+            $this->node = \Drupal::routeMatch()->getParameter('node');
+         }
 
          /**
           * {@inheritdoc}
           */
 
          public function build()
-         {
-            $node = \Drupal::routeMatch()->getParameter('node');
-            $service = \Drupal::service('count_days');
-             
+         {   
              return[
-                 '#markup' => $this->$service->getEventCountdown($node) ,
+                 '#markup' => $this->countDays->getEventCountdown($this->node) ,
                  '#cache'=>[
                      'max-age' => 0,
                  ],
              ];
             }
-
-              
-            // public static function getEventCountdown($node) 
-            // {
-            //     $current_time = \Drupal::time()->getCurrentTime();
-            //     $dateCurrent = date('d-m-Y H:i:s', $current_time);
-            //     $dateCurrectComapre = date('d-m-Y', $current_time);
-
-                
-            //     if ($node instanceof NodeInterface) 
-            //     {
-            //       $nid = $node->id();
-            //       $title = $node->title->value;
-            //       $date = $node->field_date->date->format('d-m-Y H:i:s');
-            //       $startTimeStamp = strtotime($dateCurrent);
-            //       $endTimeStamp = strtotime($date);
-            //       $dateDay = 86400;
-            //       $dateHours = 3600;
-            //       $timeDiffDay = abs($endTimeStamp - $startTimeStamp);
-            //       $numberHours = $timeDiffDay / $dateHours;
-            //       $numberHours = intval($numberHours);
-            //       $dayOne = $node->field_date->date->format('d-m-Y');
-                
-                
-            //     if($endTimeStamp > $startTimeStamp)
-            //     {
-            //         $timeDiff = abs($endTimeStamp - $startTimeStamp);
-            //         $numberDays = $timeDiff / $dateDay;
-            //         $numberDays = intval($numberDays);
-            //         if(($numberHours < $dateHours) && ($numberDays == 0)){
-            //             if ($dayOne == $dateCurrectComapre) {
-            //                 return t("This event is happening today");
-            //             }else{
-            //                 return t("This event is happening next day");
-            //             }
-            //         }else {
-            //             return t($numberDays . " days left until event starts");
-            //         }
-            //     }  
-            //     elseif($endTimeStamp < $startTimeStamp){
-            //              return t("This event already passed");
-            //         }
-            //         else
-            //         {
-            //             return " ";
-            //         }
-            
-            //  }
-            // }
     }
 ?>
--- src/Services/EventCountdown.php
+++ src/Services/EventCountdown.php
@@ -1,30 +1,15 @@
 <?php
 namespace Drupal\count_days\Services;
-use Drupal\Core\Controller\ControllerBase;
 
-// use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
-// use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\node\NodeInterface;
 
 class EventCountdown
 {
-    protected $node;
-    /**
-   * {@inheritdoc}
-   */
-  public function __construct($node) {
-    $this->node = $node;
-  }
-  public static function getEventCountdown($node) 
-{
+  public function getEventCountdown($node){
     $current_time = \Drupal::time()->getCurrentTime();
     $dateCurrent = date('d-m-Y H:i:s', $current_time);
     $dateCurrectComapre = date('d-m-Y', $current_time);
-    $node = \Drupal::routeMatch()->getParameter('node');
-     $service = \Drupal::service('count_days.time');
-     print_r($service);
-    
-    if ($node instanceof NodeInterface) 
-    {
+    if ($node instanceof NodeInterface) {
       $nid = $node->id();
       $title = $node->title->value;
       $date = $node->field_date->date->format('d-m-Y H:i:s');
@@ -37,30 +22,29 @@ class EventCountdown
       $numberHours = intval($numberHours);
       $dayOne = $node->field_date->date->format('d-m-Y');
     
-    
-    if($endTimeStamp > $startTimeStamp)
-    {
-        $timeDiff = abs($endTimeStamp - $startTimeStamp);
-        $numberDays = $timeDiff / $dateDay;
-        $numberDays = intval($numberDays);
-        if(($numberHours < $dateHours) && ($numberDays == 0)){
-            if ($dayOne == $dateCurrectComapre) {
-                return t("This event is happening today");
-            }else{
-                return t("This event is happening next day");
+        if($endTimeStamp > $startTimeStamp)
+        {
+            $timeDiff = abs($endTimeStamp - $startTimeStamp);
+            $numberDays = $timeDiff / $dateDay;
+            $numberDays = intval($numberDays);
+            if(($numberHours < $dateHours) && ($numberDays == 0)){
+                if ($dayOne == $dateCurrectComapre) {
+                    return t("This event is happening today");
+                }else{
+                    return t("This event is happening next day");
+                }
             }
-        }else {
-            return t($numberDays . " days left until event starts");
-        }
-    }  
-    elseif($endTimeStamp < $startTimeStamp){
+            else {
+                return t($numberDays . " days left until event starts");
+            }
+        }  
+        elseif($endTimeStamp < $startTimeStamp){
              return t("This event already passed");
         }
-        else
-        {
+        else{
             return " ";
         }
 
- }
-}
+    }
+  }
 }
